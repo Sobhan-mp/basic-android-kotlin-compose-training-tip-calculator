4a9cc540d8f25decae3327c672f28168b9d834b3
Sobhan Mohammadpour <sobhanmohamadpour@gmail.com>
2023-11-20 12:38:15 +0100

    add: Tip calculator test

--- app/src/androidTest/java/com/example/tiptime/TipCalculatorTest.kt
+++ app/src/androidTest/java/com/example/tiptime/TipCalculatorTest.kt
@@ -1,4 +1,20 @@
 package com.example.tiptime
 
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import java.text.NumberFormat
+
 class TipCalculatorTest {
+
+    @Test
+    fun tipCalculator_20PercentNoRoundUp(){
+        //Given
+        val amount = 20.00
+        val tipPercent = 20.00
+        val expectedValue = NumberFormat.getCurrencyInstance().format(4)
+        //When
+        val realValue = calculateTip(amount, tipPercent, false)
+        //Then
+        assertEquals(expectedValue, realValue)
+    }
 }
